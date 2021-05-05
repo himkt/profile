3de09ccc6a186cd19c1b24e9f495848b1e8b65fd
Makoto Hiramatsu <himkt@cookpad.com>
2021-05-05 23:34:18 +0900

    Update header

--- src/organisms/hero.tsx
+++ src/organisms/hero.tsx
@@ -36,14 +36,10 @@ class Hero extends React.Component<any> {
                     <p className="title is-5" style={{color: 'hsl(0, 0%, 29%)'}}>Social Accounts:</p>
                     <div className="content">
                       <span style={publication}><a href="https://twitter.com/himkt" target="_brank"><i className="fab fa-lg fa-twitter"></i></a></span>
-                      <span style={publication}><a href="https://www.instagram.com/himamako/" target="_brank"><i className="fab fa-lg fa-instagram"></i></a></span>
                       <span style={publication}><a href="https://github.com/himkt" target="_brank"><i className="fab fa-lg fa-github"></i></a></span>
-                      <span style={publication}><a href="https://speakerdeck.com/himkt" target="_brank"><i className="fab fa-lg fa-speaker-deck"></i></a></span>
-                      <span style={publication}><a href="https://himkt.medium.com" target="_brank"><i className="fab fa-lg fa-medium"></i></a></span>
-                      <span style={publication}><a href="http://qiita.com/klis" target="_brank"><i className="fas fa-lg fa-blog"></i></a></span>
-                      <span style={publication}><a href="http://note.com/himkt" target="_brank"><i className="fas fa-lg fa-sticky-note"></i></a></span>
+                      <span style={publication}><a href="https://www.linkedin.com/in/himkt" target="_brank"><i className="fab fa-lg fa-linkedin"></i></a></span>
                       <span style={publication}><a href="https://scholar.google.com/citations?user=48jLQIwAAAAJ&hl=en&oi=ao" target="_brank"><i className="fas fa-lg fa-graduation-cap"></i></a></span>
-                      <span style={publication}><a href="http://facebook.com/makoto.hiramatsu.001" target="_brank"><i className="fab fa-lg fa-facebook"></i></a></span>
+                      <span style={publication}><a href="https://www.instagram.com/himamako/" target="_brank"><i className="fab fa-lg fa-instagram"></i></a></span>
                     </div>
                     <p className="title is-5" style={{color: 'hsl(0, 0%, 29%)'}}>Links:</p>
                     <div className="content">
