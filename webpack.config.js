648d6e0d6549fbf9fecc1314656e89cf9d6f75fe
Makoto Hiramatsu <himkt@cookpad.com>
2021-05-05 20:20:20 +0900

    Upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,55 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.tsx',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.tsx']
+  },
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
-
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
-  ],
-
-  devtool: 'source-map',
-
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+  optimization: {
+    minimize: false,
   },
-
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+              ]
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.png$/,
+        use: ['url-loader']
+      }
+    ]
+  },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: {
       index: 'index.html'
     },
     port: '3000'
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        }
-      },
-      {
-        test: /\.tsx?$/,
-        loaders: 'ts-loader'
-      },
-      {
-        test: /\.css$/,
-        loaders: ['css-loader'],
-      },
-      {
-        test: /\.png$/,
-        loaders: 'url-loader'
-      },
-    ]
   }
-};
+}
